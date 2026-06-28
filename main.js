e0d1a729faeeee8d1f91c723abb6940952c63bed
Tushar Chowdhari <tusharv.chowdhari@gmail.com>
2026-06-29 01:07:29 +0530

    Initial project structure

---
+++ main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    minWidth: 1100,
+    minHeight: 700,
+    autoHideMenuBar: true,
+    title: "Chowdhari Ads Daily Register",
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  win.loadFile(path.join(__dirname, "index.html"));
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
